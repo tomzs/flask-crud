5abc53b36cc0157f7f57f4b86766e3c76bb92431
Toms <11509310+tomzs@users.noreply.github.com>
2019-03-02 02:19:38 +0200

    Templates, routes and stuff

---
+++ posts.py
@@ -0,0 +1,19 @@
+def Posts():
+    posts = [
+        {
+            "id": 1,
+            "name": "First post",
+            "content": "This is the first post made here, historians will look down on this moment forever and ever.",
+            "author": "admin",
+            "creation_date": "1-3-2019",
+        },
+        {
+            "id": 2,
+            "name": "Second post",
+            "content": "Second post is not as impressive as the first one.",
+            "author": "admin",
+            "creation_date": "1-3-2019",
+        }
+    ]
+
+    return posts
